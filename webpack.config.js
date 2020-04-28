4baa2ed4e8ddd527fd073f203927ab877c54a939
ees <eescobar@computin.cl>
2020-04-28 19:48:44 -0400

    Inicializacion del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebPackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin()
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false,
+                },
+            }, 
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyWebPackPlugin([{
+            from: 'src/assets', to: 'assets/'
+        }])
+    ]
+}
